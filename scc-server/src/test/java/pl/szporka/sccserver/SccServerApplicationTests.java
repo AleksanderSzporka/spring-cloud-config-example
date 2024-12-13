b91bf152d46ea8970dd8bcc76d2aa1fc96027047
AleksanderSzporka <a.szporka@gmail.com>
2024-12-13 20:54:03 +0100

    feat: initial

---
+++ scc-server/src/test/java/pl/szporka/sccserver/SccServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szporka.sccserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SccServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
