83a258cd9892b38bcc8c8de94d2b8e7bdd7409a7
AleksanderSzporka <a.szporka@gmail.com>
2024-12-16 15:47:15 +0100

    feat: initial

---
+++ scc-client/src/main/java/pl/szporka/sccclient/SccClientConfig.java
@@ -0,0 +1,27 @@
+package pl.szporka.sccclient;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SccClientConfig {
+
+  @Value("${spring.application.name}")
+  private String appName;
+
+  @Value("${spring.profiles.active:}")
+  private String profiles;
+
+  @Value("${scc.message}")
+  private String sccMessage;
+
+
+
+  @PostConstruct
+  public void init() {
+    System.out.println("Application name: " + appName);
+    System.out.println("Profiles: " + profiles);
+    System.out.println("SCC message: " + sccMessage);
+  }
+}
---
+++ scc-client/src/test/java/pl/szporka/sccclient/SccClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szporka.sccclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SccClientApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
